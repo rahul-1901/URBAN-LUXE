b3f721f63c1d841770554cdb65dc16fca4d07cb2
Rahul <147478589+iitian12@users.noreply.github.com>
2025-05-05 12:02:51 +0530

    addedEnv

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   const GoogleAuthWrapper = () => {
     return ( 
-      <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_ID}>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_ID}>
         <Login></Login>
       </GoogleOAuthProvider>
     )
